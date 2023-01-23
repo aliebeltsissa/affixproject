5adf34abfba90da43ab150adf80c4bae40767a88
Anna Liebelt <aliebelt@sissa.it>
2023-01-23 20:19:59 +0100

    Trying to set 3-letter comparison

--- intersection_function.py
+++ intersection_function.py
@@ -68,27 +68,60 @@ def repeats_check2(lst1, lst2):
     def intersection(lst1, lst2):
         lst3 = [value for value in lst1 if value in lst2]
         return lst3
-    intersections = []
-    test_lst = []
+    test_lst1 = []
+    for i in lst11:
+        n = 0
+        while 3+n <= length1:
+            test_word = i[n:(3+n)]
+            test_lst1 += [test_word]
+            n +=1
+        else:
+            n = 0
+            continue
+    n = 0
+    test_lst2 = []
     ints = []
+    intersections = []
     for j in lst22:
         n = 0
-        while length1+n <= length2:
-            test_word = j[n:(length1+n)]
-            test_lst += [test_word]
-            ints = intersection(test_lst, lst11)
+        while 3+n <= length2:
+            test_word = j[n:(3+n)]
+            test_lst2 += [test_word]
+            ints = intersection(test_lst1, test_lst2)
             intersections += ints
             ints = []
-            test_lst = []
+            test_lst2 = []
             n += 1
         else:
             n = 0
             continue
-    intersections = [''.join(i) for i in intersections]
-    print(intersections)
-    lst1b = [x for x in lst1 if x not in intersections]
-    print(lst1b)
+    intersections2 = [''.join(i) for i in intersections]
+    print(intersections2)
+    lst1a = intersection(test_lst1, intersections)
+    print(lst1a)
+    test_lst = []
+    lst1c = []
+    for i in lst11:
+        n = 0
+        while 3+n <= length1:
+            test_word = i[n:(3+n)]
+            print(test_word)
+            #if test_word in lst1a:
+            #    lst11 = lst11 - test_word
+            l3 = [x for x in lst11 if x not in test_word]
+            #test_lst += [test_word]
+            #lst1b = intersection(test_lst, lst1a)
+            #lst11.remove(lst1b)
+            #lst1b = []
+            #test_lst = []
+            n += 1
+        else:
+            n = 0
+            continue
+    print(l3)
+    #lst1c = [''.join(i) for i in lst11]
+    #print(lst1c)
     
-example1 = ["wol", "log", "hat", "bye", "but"]
-example2 = ["hell", "logg", "hats", "drug", "wolf"]
+example1 = ["wolr", "logs", "hate", "byeb", "buts"]
+example2 = ["heller", "loggy", "hatsy", "drugsy", "wolfer"]
 repeats_check2(example1, example2)
