61b93c41b07261133622827cf633184dba936e32
Anna Liebelt <aliebelt@sissa.it>
2023-04-03 19:18:47 +0200

    Looked at initial pilot data

--- exercise_dataanalysis.R
+++ exercise_dataanalysis.R
@@ -7,10 +7,8 @@ data4 <- read.table("./Participant_4/sbj4_responses.tsv",header=T,sep=",");
 data5 <- read.table("./Participant_5/sbj5_responses.tsv",header=T,sep=",");
 data6 <- read.table("./Participant_6/sbj6_responses.tsv",header=T,sep=",");
 data7 <- read.table("./Participant_7/sbj7_responses.tsv",header=T,sep=",");
-data8 <- read.table("./Participant_8/sbj8_responses.tsv",header=T,sep=",");
-data9 <- read.table("./Participant_9/sbj9_responses.tsv",header=T,sep=",");
-data10 <- read.table("./Participant_10/sbj10_responses.tsv",header=T,sep=",");
-data <- rbind(data1,data2,data3,data4,data5,data6,data7,data8,data9,data10);
+data <- rbind(data1,data2,data3,data4,data5,data6,data7);
+data_clean <- rbind(data1,data2,data3,data5,data6,data7);
 data$response <- as.factor(data$response);
 data$condition <- as.factor(data$condition);
 dim(data1);
@@ -39,6 +37,24 @@ data$strictfilter[data$RT<25 | data$RT>150] <- FALSE;
 data$lenientfilter <- TRUE;
 data$lenientfilter[data$RT>175] <- FALSE;
 summary(data);
-plot(density(data$RT),xlim=c(0,250),ylim=c(0,.016));
-lines(density(data$RT[data$strictfilter],na.rm=T),col="#0073C2FF");
-lines(density(data$RT[data$lenientfilter],na.rm=T),col="#ad2b07")
+
+# RT plot with all participants
+plot(density(data1$RT),xlim=c(0,1750),ylim=c(0,0.0055),xlab="RTs",main="",xaxt = "n",col="cadetblue");
+axis(1, at = c(0,200,400,600,800,1000,1200,1400,1600,1800))
+lines(density(data2$RT),col="coral");
+lines(density(data3$RT),col="darkgoldenrod");
+lines(density(data4$RT),col="darkgreen");
+lines(density(data5$RT),col="indianred4");
+lines(density(data6$RT),col="lightslateblue");
+lines(density(data7$RT),col="navy");
+legend("topright",title="Participant:",c("1","2","3","4","5","6","7"),fill=c("cadetblue","coral","darkgoldenrod","darkgreen","indianred4","lightslateblue","navy"),bty = "n",
+       cex=0.75,y.intersp=0.5)
+
+# scores & RTs with all participants
+scores <- list(42.5,57.5,35.0,42.5,50.0,37.5,52.5);
+mean_RTs <- list(344.5735,461.22575,187.264,505.766,260.111,211.34025,530.5685);
+plot(unlist(mean_RTs),unlist(scores),pch=16)
+# scores & RTs without participant 4
+scores2 <- list(42.5,57.5,35.0,50.0,37.5,52.5);
+mean_RTs2 <- list(344.5735,461.22575,187.264,260.111,211.34025,530.5685);
+plot(unlist(mean_RTs2),unlist(scores2),pch=16)
