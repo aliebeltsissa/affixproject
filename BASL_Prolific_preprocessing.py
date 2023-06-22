e96e7b50b7c94a3e3d4bbb8593ace75eabec138f
Anna Liebelt <aliebelt@sissa.it>
2023-06-22 16:01:11 +0200

    Finished writing preprocessing code for BLP

--- BASL_Prolific_preprocessing.py
+++ BASL_Prolific_preprocessing.py
@@ -1,7 +1,7 @@
 from os import listdir, chdir
 from os.path import isfile
 chdir("C:\\Users\\annal\\Downloads\\practice") # set working directory to responses folder
-allfiles = [f for f in listdir() if isfile(f)]
+allfiles = [f for f in listdir() if isfile(f)] # get all file names
 
 import pandas as pd
 all_data = []
@@ -20,7 +20,7 @@ for f in allfiles:
     
 import json
 BLP_data = []
-for x in range(len(all_data)):
+for x in range(len(all_data)): # extract BLP responses
     participant_data = all_data[x]
     participant_BLP_data = []
     for y in participant_data.keys():
@@ -32,10 +32,24 @@ for x in range(len(all_data)):
     BLP_data.append(participant_BLP_data)
     
 def BLP_preprocessing(BLP_file):
-    AoA12_mapping = {"Non so questa lingua": 0, "20+": 0, "19": 1, "18": 2, "17": 3, "16": 4, "15": 5, "14": 6, "13": 7, "12": 8, "11": 9, "10": 10, "9": 11, "8": 12, "7": 13, "6": 14, "5": 15, "4": 16, "3": 17, "2": 18, "1": 19, "Dalla nascita": 20, "Da quando ne ho ricordo": 20, "Ancora no": 0}
+    '''
+    Function taking a DataFrame of BLP data & scoring responses
+
+    Parameters
+    ----------
+    BLP_file : DataFrame
+        DESCRIPTION.
+
+    Returns
+    -------
+    BLP_data2 : DataFrame
+        Sorted & scored DataFrame with a line per participant.
+    '''
+    # replacing individual responses with their equivalent score
+    AoA12_mapping = {"20+": 0, "19": 1, "18": 2, "17": 3, "16": 4, "15": 5, "14": 6, "13": 7, "12": 8, "11": 9, "10": 10, "9": 11, "8": 12, "7": 13, "6": 14, "5": 15, "4": 16, "3": 17, "2": 18, "1": 19, "0": 0, "Dalla nascita": 20, "Da quando ne ho ricordo": 20, "notyet": 0}
     BLP_data2 = BLP_file.replace({"AoAL1": AoA12_mapping, "AoAL2": AoA12_mapping, "AoAL3": AoA12_mapping, "AoAL4": AoA12_mapping})
     BLP_data3 = BLP_data2.replace({"AoAgioL1": AoA12_mapping, "AoAgioL2": AoA12_mapping, "AoAgioL3": AoA12_mapping, "AoAgioL4": AoA12_mapping})
-    AoA_mapping = {"Non so questa lingua": 0, "20+": 20, "19": 19, "18": 18, "17": 17, "16": 16, "15": 15, "14": 14, "13": 13, "12": 12, "11": 11, "10": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2, "1": 1, "0": 0}
+    AoA_mapping = {"20+": 20, "19": 19, "18": 18, "17": 17, "16": 16, "15": 15, "14": 14, "13": 13, "12": 12, "11": 11, "10": 10, "9": 9, "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2, "1": 1, "0": 0}
     BLP_data2 = BLP_data3.replace({"anniInstrL1": AoA_mapping, "anniInstrL2": AoA_mapping, "anniInstrL3": AoA_mapping, "anniInstrL4": AoA_mapping})
     BLP_data3 = BLP_data2.replace({"anniPaeseL1": AoA_mapping, "anniPaeseL2": AoA_mapping, "anniPaeseL3": AoA_mapping, "anniPaeseL4": AoA_mapping})
     BLP_data2 = BLP_data3.replace({"anniFamigliaL1": AoA_mapping, "anniFamigliaL2": AoA_mapping, "anniFamigliaL3": AoA_mapping, "anniFamigliaL4": AoA_mapping})
@@ -48,104 +62,150 @@ def BLP_preprocessing(BLP_file):
     BLP_data3 = BLP_data2.replace({"PercStessoL1": percent_mapping, "PercStessoL2": percent_mapping, "PercStessoL3": percent_mapping, "PercStessoL4": percent_mapping})
     BLP_data2 = BLP_data3.replace({"PercCalcoliL1": percent_mapping, "PercCalcoliL2": percent_mapping, "PercCalcoliL3": percent_mapping, "PercCalcoliL4": percent_mapping})
     
+    # summing up scores per section per language
     all_history_scoreL1 = []
     all_history_scoreL2 = []
     all_history_scoreL3 = []
     all_history_scoreL4 = []
-    for sbj in BLP_data2.index:
-        history_scoreL1 = (BLP_data2["AoAL1"][sbj] + BLP_data2["AoAgioL1"][sbj] + BLP_data2["anniInstrL1"][sbj] + BLP_data2["anniPaeseL1"][sbj] + BLP_data2["anniFamigliaL1"][sbj] + BLP_data2["anniLavoroL1"][sbj])*0.454
-        history_scoreL2 = (BLP_data2["AoAL2"][sbj] + BLP_data2["AoAgioL2"][sbj] + BLP_data2["anniInstrL2"][sbj] + BLP_data2["anniPaeseL2"][sbj] + BLP_data2["anniFamigliaL2"][sbj] + BLP_data2["anniLavoroL2"][sbj])*0.454
-        history_scoreL3 = (BLP_data2["AoAL3"][sbj] + BLP_data2["AoAgioL3"][sbj] + BLP_data2["anniInstrL3"][sbj] + BLP_data2["anniPaeseL3"][sbj] + BLP_data2["anniFamigliaL3"][sbj] + BLP_data2["anniLavoroL3"][sbj])*0.454
-        history_scoreL4 = (BLP_data2["AoAL4"][sbj] + BLP_data2["AoAgioL4"][sbj] + BLP_data2["anniInstrL4"][sbj] + BLP_data2["anniPaeseL4"][sbj] + BLP_data2["anniFamigliaL4"][sbj] + BLP_data2["anniLavoroL4"][sbj])*0.454
-        all_history_scoreL1.append(history_scoreL1)
-        all_history_scoreL2.append(history_scoreL2)
-        all_history_scoreL3.append(history_scoreL3)
-        all_history_scoreL4.append(history_scoreL4)
+    BLP_data2 = BLP_data2.astype({"AoAL1":"int","AoAgioL1":"int","anniInstrL1":"int","anniPaeseL1":"int","anniFamigliaL1":"int","anniLavoroL1":"int"})
+    history_scoreL1 = (BLP_data2["AoAL1"][0] + BLP_data2["AoAgioL1"][0] + BLP_data2["anniInstrL1"][0] + BLP_data2["anniPaeseL1"][0] + BLP_data2["anniFamigliaL1"][0] + BLP_data2["anniLavoroL1"][0])*0.454
+    try:
+        BLP_data2 = BLP_data2.astype({"AoAL2":"int","AoAgioL2":"int","anniInstrL2":"int","anniPaeseL2":"int","anniFamigliaL2":"int","anniLavoroL2":"int"})
+        history_scoreL2 = (BLP_data2["AoAL2"][0] + BLP_data2["AoAgioL2"][0] + BLP_data2["anniInstrL2"][0] + BLP_data2["anniPaeseL2"][0] + BLP_data2["anniFamigliaL2"][0] + BLP_data2["anniLavoroL2"][0])*0.454
+    except KeyError:
+        history_scoreL2 = 0            
+    try:
+        BLP_data2 = BLP_data2.astype({"AoAL3":"int","AoAgioL3":"int","anniInstrL3":"int","anniPaeseL3":"int","anniFamigliaL3":"int","anniLavoroL3":"int"})
+        history_scoreL3 = (BLP_data2["AoAL3"][0] + BLP_data2["AoAgioL3"][0] + BLP_data2["anniInstrL3"][0] + BLP_data2["anniPaeseL3"][0] + BLP_data2["anniFamigliaL3"][0] + BLP_data2["anniLavoroL3"][0])*0.454
+    except KeyError:
+        history_scoreL3 = 0  
+    try:
+        BLP_data2 = BLP_data2.astype({"AoAL4":"int","AoAgioL4":"int","anniInstrL4":"int","anniPaeseL4":"int","anniFamigliaL4":"int","anniLavoroL4":"int"})
+        history_scoreL4 = (BLP_data2["AoAL4"][0] + BLP_data2["AoAgioL4"][0] + BLP_data2["anniInstrL4"][0] + BLP_data2["anniPaeseL4"][0] + BLP_data2["anniFamigliaL4"][0] + BLP_data2["anniLavoroL4"][0])*0.454
+    except KeyError:
+        history_scoreL4 = 0  
+    all_history_scoreL1.append(history_scoreL1)
+    all_history_scoreL2.append(history_scoreL2)
+    all_history_scoreL3.append(history_scoreL3)
+    all_history_scoreL4.append(history_scoreL4)
         
     all_use_scoreL1 = []
     all_use_scoreL2 = []
     all_use_scoreL3 = []
     all_use_scoreL4 = []
-    for sbj in BLP_data2.index:
-        use_scoreL1 = (BLP_data2["PercAmiciL1"][sbj] + BLP_data2["PercFamigliaL1"][sbj] + BLP_data2["PercLavoroL1"][sbj] + BLP_data2["PercStessoL1"][sbj] + BLP_data2["PercCalcoliL1"][sbj])*1.09
-        use_scoreL2 = (BLP_data2["PercAmiciL2"][sbj] + BLP_data2["PercFamigliaL2"][sbj] + BLP_data2["PercLavoroL2"][sbj] + BLP_data2["PercStessoL2"][sbj] + BLP_data2["PercCalcoliL2"][sbj])*1.09
-        use_scoreL3 = (BLP_data2["PercAmiciL3"][sbj] + BLP_data2["PercFamigliaL3"][sbj] + BLP_data2["PercLavoroL3"][sbj] + BLP_data2["PercStessoL3"][sbj] + BLP_data2["PercCalcoliL3"][sbj])*1.09
-        use_scoreL4 = (BLP_data2["PercAmiciL4"][sbj] + BLP_data2["PercFamigliaL4"][sbj] + BLP_data2["PercLavoroL4"][sbj] + BLP_data2["PercStessoL4"][sbj] + BLP_data2["PercCalcoliL4"][sbj])*1.09
-        all_use_scoreL1.append(use_scoreL1)
-        all_use_scoreL2.append(use_scoreL2)
-        all_use_scoreL3.append(use_scoreL3)
-        all_use_scoreL4.append(use_scoreL4)
+    use_scoreL1 = (BLP_data2["PercAmiciL1"][0] + BLP_data2["PercFamigliaL1"][0] + BLP_data2["PercLavoroL1"][0] + BLP_data2["PercStessoL1"][0] + BLP_data2["PercCalcoliL1"][0])*1.09
+    try:
+        use_scoreL2 = (BLP_data2["PercAmiciL2"][0] + BLP_data2["PercFamigliaL2"][0] + BLP_data2["PercLavoroL2"][0] + BLP_data2["PercStessoL2"][0] + BLP_data2["PercCalcoliL2"][0])*1.09
+    except KeyError:
+        use_scoreL2 = 0
+    try:
+        use_scoreL3 = (BLP_data2["PercAmiciL3"][0] + BLP_data2["PercFamigliaL3"][0] + BLP_data2["PercLavoroL3"][0] + BLP_data2["PercStessoL3"][0] + BLP_data2["PercCalcoliL3"][0])*1.09
+    except KeyError:
+        use_scoreL3 = 0
+    try:
+        use_scoreL4 = (BLP_data2["PercAmiciL4"][0] + BLP_data2["PercFamigliaL4"][0] + BLP_data2["PercLavoroL4"][0] + BLP_data2["PercStessoL4"][0] + BLP_data2["PercCalcoliL4"][0])*1.09
+    except KeyError:
+        use_scoreL4 = 0
+    all_use_scoreL1.append(use_scoreL1)
+    all_use_scoreL2.append(use_scoreL2)
+    all_use_scoreL3.append(use_scoreL3)
+    all_use_scoreL4.append(use_scoreL4)
         
     all_proficiency_scoreL1 = []
     all_proficiency_scoreL2 = []
     all_proficiency_scoreL3 = []
     all_proficiency_scoreL4 = []
-    for sbj in BLP_data2.index:
-        # make all scores (currently strs) into ints
-        proficiency_scoreL1 = (BLP_data2["ProfParlaL1"][sbj] + BLP_data2["ProfCapisceL1"][sbj] + BLP_data2["ProfLeggeL1"][sbj] + BLP_data2["ProfScriveL1"][sbj])*2.27
-        proficiency_scoreL2 = (BLP_data2["ProfParlaL2"][sbj] + BLP_data2["ProfCapisceL2"][sbj] + BLP_data2["ProfLeggeL2"][sbj] + BLP_data2["ProfScriveL2"][sbj])*2.27
-        proficiency_scoreL3 = (BLP_data2["ProfParlaL3"][sbj] + BLP_data2["ProfCapisceL3"][sbj] + BLP_data2["ProfLeggeL3"][sbj] + BLP_data2["ProfScriveL3"][sbj])*2.27
-        proficiency_scoreL4 = (BLP_data2["ProfParlaL4"][sbj] + BLP_data2["ProfCapisceL4"][sbj] + BLP_data2["ProfLeggeL4"][sbj] + BLP_data2["ProfScriveL4"][sbj])*2.27
-        all_proficiency_scoreL1.append(proficiency_scoreL1)
-        all_proficiency_scoreL2.append(proficiency_scoreL2)
-        all_proficiency_scoreL3.append(proficiency_scoreL3)
-        all_proficiency_scoreL4.append(proficiency_scoreL4)
+    BLP_data2 = BLP_data2.astype({"ProfParlaL1":"int","ProfCapisceL1":"int","ProfLeggeL1":"int","ProfScriveL1":"int"})
+    proficiency_scoreL1 = (BLP_data2["ProfParlaL1"][0] + BLP_data2["ProfCapisceL1"][0] + BLP_data2["ProfLeggeL1"][0] + BLP_data2["ProfScriveL1"][0])*2.27
+    try:
+        BLP_data2 = BLP_data2.astype({"ProfParlaL2":"int","ProfCapisceL2":"int","ProfLeggeL2":"int","ProfScriveL2":"int"})
+        proficiency_scoreL2 = (BLP_data2["ProfParlaL2"][0] + BLP_data2["ProfCapisceL2"][0] + BLP_data2["ProfLeggeL2"][0] + BLP_data2["ProfScriveL2"][0])*2.27
+    except KeyError:
+        proficiency_scoreL2 = 0
+    try:
+        BLP_data2 = BLP_data2.astype({"ProfParlaL3":"int","ProfCapisceL3":"int","ProfLeggeL3":"int","ProfScriveL3":"int"})
+        proficiency_scoreL3 = (BLP_data2["ProfParlaL3"][0] + BLP_data2["ProfCapisceL3"][0] + BLP_data2["ProfLeggeL3"][0] + BLP_data2["ProfScriveL3"][0])*2.27
+    except KeyError:
+        proficiency_scoreL3 = 0
+    try:
+        BLP_data2 = BLP_data2.astype({"ProfParlaL4":"int","ProfCapisceL4":"int","ProfLeggeL4":"int","ProfScriveL4":"int"})
+        proficiency_scoreL4 = (BLP_data2["ProfParlaL4"][0] + BLP_data2["ProfCapisceL4"][0] + BLP_data2["ProfLeggeL4"][0] + BLP_data2["ProfScriveL4"][0])*2.27
+    except KeyError:
+        proficiency_scoreL4 = 0
+    all_proficiency_scoreL1.append(proficiency_scoreL1)
+    all_proficiency_scoreL2.append(proficiency_scoreL2)
+    all_proficiency_scoreL3.append(proficiency_scoreL3)
+    all_proficiency_scoreL4.append(proficiency_scoreL4)
         
     all_attitude_scoreL1 = []
     all_attitude_scoreL2 = []
     all_attitude_scoreL3 = []
     all_attitude_scoreL4 = []
-    for sbj in BLP_data2.index:
-        attitude_scoreL1 = (BLP_data2["AttSentoStessoL1"][sbj] + BLP_data2["AttCulturaL1"][sbj] + BLP_data2["AttLivelloL1"][sbj] + BLP_data2["AttMadrelinguaL1"][sbj])*2.27
-        attitude_scoreL2 = (BLP_data2["AttSentoStessoL2"][sbj] + BLP_data2["AttCulturaL2"][sbj] + BLP_data2["AttLivelloL2"][sbj] + BLP_data2["AttMadrelinguaL2"][sbj])*2.27
-        attitude_scoreL3 = (BLP_data2["AttSentoStessoL3"][sbj] + BLP_data2["AttCulturaL3"][sbj] + BLP_data2["AttLivelloL3"][sbj] + BLP_data2["AttMadrelinguaL3"][sbj])*2.27
-        attitude_scoreL4 = (BLP_data2["AttSentoStessoL4"][sbj] + BLP_data2["AttCulturaL4"][sbj] + BLP_data2["AttLivelloL4"][sbj] + BLP_data2["AttMadrelinguaL4"][sbj])*2.27
-        all_attitude_scoreL1.append(attitude_scoreL1)
-        all_attitude_scoreL2.append(attitude_scoreL2)
-        all_attitude_scoreL3.append(attitude_scoreL3)
-        all_attitude_scoreL4.append(attitude_scoreL4)
+    BLP_data2 = BLP_data2.astype({"AttMiStessoL1":"int","AttCulturaL1":"int","AttLivNativoL1":"int","AttMadrelinguaL1":"int"})
+    attitude_scoreL1 = (BLP_data2["AttMiStessoL1"][0] + BLP_data2["AttCulturaL1"][0] + BLP_data2["AttLivNativoL1"][0] + BLP_data2["AttMadrelinguaL1"][0])*2.27
+    try:
+        BLP_data2 = BLP_data2.astype({"AttMiStessoL2":"int","AttCulturaL2":"int","AttLivNativoL2":"int","AttMadrelinguaL2":"int"})
+        attitude_scoreL2 = (BLP_data2["AttMiStessoL2"][0] + BLP_data2["AttCulturaL2"][0] + BLP_data2["AttLivNativoL2"][0] + BLP_data2["AttMadrelinguaL2"][0])*2.27
+    except KeyError:
+        attitude_scoreL2 = 0
+    try:
+        BLP_data2 = BLP_data2.astype({"AttMiStessoL3":"int","AttCulturaL3":"int","AttLivNativoL3":"int","AttMadrelinguaL3":"int"})
+        attitude_scoreL3 = (BLP_data2["AttMiStessoL3"][0] + BLP_data2["AttCulturaL3"][0] + BLP_data2["AttLivNativoL3"][0] + BLP_data2["AttMadrelinguaL3"][0])*2.27
+    except KeyError:
+        attitude_scoreL3 = 0
+    try:
+        BLP_data2 = BLP_data2.astype({"AttMiStessoL4":"int","AttCulturaL4":"int","AttLivNativoL4":"int","AttMadrelinguaL4":"int"})
+        attitude_scoreL4 = (BLP_data2["AttMiStessoL4"][0] + BLP_data2["AttCulturaL4"][0] + BLP_data2["AttLivNativoL4"][0] + BLP_data2["AttMadrelinguaL4"][0])*2.27
+    except KeyError:
+        attitude_scoreL4 = 0
+    all_attitude_scoreL1.append(attitude_scoreL1)
+    all_attitude_scoreL2.append(attitude_scoreL2)
+    all_attitude_scoreL3.append(attitude_scoreL3)
+    all_attitude_scoreL4.append(attitude_scoreL4)
         
-    attention_mapping = {0: "false", 1: "false", 2: "true", 3: "false", 4: "false", 5: "false", 6: "false"}
-    BLP_data3 = BLP_data2.replace({"AttentionCheck": attention_mapping})
+    attention_mapping = {"0": "false", "1": "false", "2": "true", "3": "false", "4": "false", "5": "false", "6": "false"}
+    BLP_data3 = BLP_data2.replace({"AttentionL1": attention_mapping})
+    BLP_data2 = BLP_data3.replace({"AttentionL2": attention_mapping})
+    BLP_data3 = BLP_data2.replace({"AttentionL3": attention_mapping})
+    BLP_data2 = BLP_data3.replace({"AttentionL4": attention_mapping})
     
-    BLP_data3["HistoryL1Score"] = all_history_scoreL1
-    BLP_data3["HistoryL2Score"] = all_history_scoreL2
-    BLP_data3["HistoryL3Score"] = all_history_scoreL3
-    BLP_data3["HistoryL4Score"] = all_history_scoreL4
-    BLP_data3["UseL1Score"] = all_use_scoreL1
-    BLP_data3["UseL2Score"] = all_use_scoreL2
-    BLP_data3["UseL3Score"] = all_use_scoreL3
-    BLP_data3["UseL4Score"] = all_use_scoreL4
-    BLP_data3["ProficiencyL1Score"] = all_proficiency_scoreL1
-    BLP_data3["ProficiencyL2Score"] = all_proficiency_scoreL2
-    BLP_data3["ProficiencyL3Score"] = all_proficiency_scoreL3
-    BLP_data3["ProficiencyL4Score"] = all_proficiency_scoreL4
-    BLP_data3["AttitudeL1Score"] = all_attitude_scoreL1
-    BLP_data3["AttitudeL2Score"] = all_attitude_scoreL2
-    BLP_data3["AttitudeL3Score"] = all_attitude_scoreL3
-    BLP_data3["AttitudeL4Score"] = all_attitude_scoreL4
+    BLP_data2["HistoryL1Score"] = all_history_scoreL1
+    BLP_data2["HistoryL2Score"] = all_history_scoreL2
+    BLP_data2["HistoryL3Score"] = all_history_scoreL3
+    BLP_data2["HistoryL4Score"] = all_history_scoreL4
+    BLP_data2["UseL1Score"] = all_use_scoreL1
+    BLP_data2["UseL2Score"] = all_use_scoreL2
+    BLP_data2["UseL3Score"] = all_use_scoreL3
+    BLP_data2["UseL4Score"] = all_use_scoreL4
+    BLP_data2["ProficiencyL1Score"] = all_proficiency_scoreL1
+    BLP_data2["ProficiencyL2Score"] = all_proficiency_scoreL2
+    BLP_data2["ProficiencyL3Score"] = all_proficiency_scoreL3
+    BLP_data2["ProficiencyL4Score"] = all_proficiency_scoreL4
+    BLP_data2["AttitudeL1Score"] = all_attitude_scoreL1
+    BLP_data2["AttitudeL2Score"] = all_attitude_scoreL2
+    BLP_data2["AttitudeL3Score"] = all_attitude_scoreL3
+    BLP_data2["AttitudeL4Score"] = all_attitude_scoreL4
     
     all_L1_scores = []
     all_L2_scores = []
     all_L3_scores = []
     all_L4_scores = []
-    for sbj in BLP_data3.index:
-        L1_score = BLP_data3["HistoryL1Score"][sbj] + BLP_data3["UseL1Score"][sbj] + BLP_data3["ProficiencyL1Score"][sbj] + BLP_data3["AttitudeL1Score"][sbj]
-        L2_score = BLP_data3["HistoryL2Score"][sbj] + BLP_data3["UseL2Score"][sbj] + BLP_data3["ProficiencyL2Score"][sbj] + BLP_data3["AttitudeL2Score"][sbj]
-        L3_score = BLP_data3["HistoryL3Score"][sbj] + BLP_data3["UseL3Score"][sbj] + BLP_data3["ProficiencyL3Score"][sbj] + BLP_data3["AttitudeL3Score"][sbj]
-        L4_score = BLP_data3["HistoryL4Score"][sbj] + BLP_data3["UseL4Score"][sbj] + BLP_data3["ProficiencyL4Score"][sbj] + BLP_data3["AttitudeL4Score"][sbj]
-        all_L1_scores.append(L1_score)
-        all_L2_scores.append(L2_score)
-        all_L3_scores.append(L3_score)
-        all_L4_scores.append(L4_score)
+    L1_score = BLP_data2["HistoryL1Score"][0] + BLP_data2["UseL1Score"][0] + BLP_data2["ProficiencyL1Score"][0] + BLP_data2["AttitudeL1Score"][0]
+    L2_score = BLP_data2["HistoryL2Score"][0] + BLP_data2["UseL2Score"][0] + BLP_data2["ProficiencyL2Score"][0] + BLP_data2["AttitudeL2Score"][0]
+    L3_score = BLP_data2["HistoryL3Score"][0] + BLP_data2["UseL3Score"][0] + BLP_data2["ProficiencyL3Score"][0] + BLP_data2["AttitudeL3Score"][0]
+    L4_score = BLP_data2["HistoryL4Score"][0] + BLP_data2["UseL4Score"][0] + BLP_data2["ProficiencyL4Score"][0] + BLP_data2["AttitudeL4Score"][0]
+    all_L1_scores.append(L1_score)
+    all_L2_scores.append(L2_score)
+    all_L3_scores.append(L3_score)
+    all_L4_scores.append(L4_score)
         
-    BLP_data3["L1Score"] = all_L1_scores
-    BLP_data3["L2Score"] = all_L2_scores
-    BLP_data3["L3Score"] = all_L3_scores
-    BLP_data3["L4Score"] = all_L4_scores
-    return BLP_data3
+    BLP_data2["L1Score"] = all_L1_scores
+    BLP_data2["L2Score"] = all_L2_scores
+    BLP_data2["L3Score"] = all_L3_scores
+    BLP_data2["L4Score"] = all_L4_scores
+    return BLP_data2
 
-all_BLP_data = pd.DataFrame.empty
+all_BLP_data = pd.DataFrame()
 for x in range(len(BLP_data)):
     participant = BLP_data[x]
     sbj_ID = participant[0]['ID']
@@ -172,3 +232,4 @@ for x in range(len(BLP_data)):
     participant_BLP_data.insert(0, 'sbj_ID', col2)
 
     participant_BLP_data_scored = BLP_preprocessing(participant_BLP_data)
+    all_BLP_data = pd.concat([all_BLP_data,participant_BLP_data_scored],axis = 0)
